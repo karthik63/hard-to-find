9ded0beaf34c6285efe482b42b733e421dbb4f95
karthik63 <twoindeed@gmail.com>
2021-10-11 14:37:01 -0500

    a

--- supervised_entity_tagging/models/net.py
+++ supervised_entity_tagging/models/net.py
@@ -16,7 +16,9 @@ class EntityTagger(nn.Module):
 
     def forward(self, encodings:transformers.BatchEncoding, labels:torch.LongTensor):
         encoded = self.pretrained_lm(**encodings)
-        print(encoded.shape)
+        print(encoded.last_hidden_state.value.shape)
+        # print(d_model)
+        # print(n_class)
         outputs = self.linear_map(encoded.last_hidden_state)
         loss = self.compute_cross_entropy(outputs, labels)
         preds = torch.argmax(outputs, dim=-1)

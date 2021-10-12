7f9c4c755483cc60a631a5aa21e037e69361ec7f
karthik63 <twoindeed@gmail.com>
2021-10-12 02:56:30 -0500

    a

--- supervised_entity_tagging/run.py
+++ supervised_entity_tagging/run.py
@@ -55,8 +55,8 @@ def main():
             iterator = tqdm(loaders["train"])
             epoch_loss = 0.
             for idx, (encodings, labels) in enumerate(iterator):
-                if idx > 10:
-                    break
+                # if idx > 10:
+                #     break
                 try:
                     encodings = encodings.to(device)
                 except Exception as e:
@@ -79,8 +79,8 @@ def main():
                 predictions = []
                 test_dataset = loaders["test"].dataset
                 for idx, (encodings, labels) in enumerate(test_iterator):
-                    if idx > 100:
-                        break
+                    # if idx > 100:
+                    #     break
                     try:
                         inputs = encodings.to(device)
                     except Exception as e:
